8a40a1110da2f6eeb4d825d03a48124b21e7c67f
alperr <alper3017@gmail.com>
2021-10-13 17:16:32 +0300

    initial commit

---
+++ synced-object.js
@@ -0,0 +1,22 @@
+// this function syncs a js object with filesystem periodically
+// this can be used as a basic database for small objects (<10kb) 
+// do not re-assign a new reference to returned 'object' variable
+// only change inner fields 
+function sync(path, sync_interval)
+{
+	const FS = require("fs");
+	if (typeof sync_interval == "undefined")
+		sync_interval = 60 * 1000;
+
+	var object = {};
+	if (FS.existsSync(path))
+		object = JSON.parse(FS.readFileSync(path, "utf8"));
+
+	setInterval(() =>
+	{
+		FS.writeFileSync(path, JSON.stringify(object), "utf8");
+	}, sync_interval);
+	return object;
+}
+
+exports.sync = sync;
