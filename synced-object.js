acceaf94b9dc3ef7cc01deab262b18e309e0d86e
alperr <alper3017@gmail.com>
2021-10-25 17:35:55 +0300

    added default value

--- synced-object.js
+++ synced-object.js
@@ -2,13 +2,16 @@
 // this can be used as a basic database for small objects (<10kb) 
 // do not re-assign a new reference to returned 'object' variable
 // only change inner fields 
-function sync(path, sync_interval)
+function sync(path, sync_interval, def)
 {
 	const FS = require("fs");
 	if (typeof sync_interval == "undefined")
 		sync_interval = 60 * 1000;
 
 	var object = {};
+	if (typeof def == "undefined")
+		object = def;
+
 	if (FS.existsSync(path))
 		object = JSON.parse(FS.readFileSync(path, "utf8"));
 
